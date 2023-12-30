21581586d34dc57534a114404fae646c1b3c70fe
Kalo2412 <77152645+Kalo2412@users.noreply.github.com>
2023-12-30 18:12:23 +0200

    Update semant.h (Score: 64)

--- pa3/semant.h
+++ pa3/semant.h
@@ -2,8 +2,10 @@
 #define SEMANT_H_
 
 #include <assert.h>
-#include <cwchar>
-#include <iostream>  
+#include <iostream>
+#include <map> 
+#include <set>
+#include <vector>
 #include "cool-tree.h"
 #include "stringtab.h"
 #include "symtab.h"
@@ -27,16 +29,30 @@ private:
   ostream& error_stream;
 
 public:
+  std::map<Symbol, Class_> m_classes;
+  std::map<Symbol, Symbol> inh_graph;
   ClassTable(Classes);
   int errors() { return semant_errors; }
   ostream& semant_error();
   ostream& semant_error(Class_ c);
   ostream& semant_error(Symbol filename, tree_node *t);
+  void add_to_class_table(Class_ c);
+  bool is_valid();
+  bool is_child(Symbol child, Symbol parent);
+  Symbol lub(Symbol c1, Symbol c2);
+  Class_ get_class(Symbol class_name);
+  Formals get_formals(Symbol class_name, Symbol method_name);
+  Symbol get_return_type(Symbol class_name, Symbol method_name);
+  Symbol get_ancestor_method_class(Symbol class_name, Symbol method_name);
+  bool check_method_signature(Symbol c1, Symbol c2, Symbol method_name);
 };
 
 struct TypeEnvironment {
-    SymbolTable<Symbol, Symbol>* O;
+  Class_ curr_class;
+  SymbolTable<Symbol, Symbol>* O;
+  ClassTableP class_table;
 };
+
 
 #endif
 
